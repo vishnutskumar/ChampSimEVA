7c6f2b674823cb5fc8fbe62c9685b424789421a2
CharlsBabu <charlespbabu@gmail.com>
2023-04-21 17:47:01 -0500

    adds plotting of extra variables in analysis script

--- analysis/plot_AgevEVA.py
+++ analysis/plot_AgevEVA.py
@@ -10,15 +10,7 @@ MAX_COUNT = 340 #number of EVA data ranges
 MAX_AGE = 307200
 PLOT_FREQ = 20 # Plot every nth EVA range
 
-
-# with open('../'+str(MAX_AGE)+'eva_ages.txt', newline='') as csvfile:
-#     reader = csv.reader(csvfile, delimiter=',')
-#     data = list(reader)
-
 data = pd.read_csv('../'+str(MAX_AGE)+'eva_ages.txt',header=None,sep=',').to_numpy()
-
-# data = np.array(data, dtype=float)
-
 
 print("Reading CSV Done \n")
 
@@ -29,13 +21,21 @@ for rec in np.arange(MIN_COUNT, MAX_COUNT, PLOT_FREQ):
         subset = data[i*MAX_AGE:((i+1)*MAX_AGE)-1,:]
         age = subset[:,0]
         eva = subset[:,1]
-        plt.plot(age, eva)
+        hit_p = subset[:,2]
+        exp_life = subset[:3]
+        opp_cost = subset[:4]
+        line_gain = subset[:5]
+
+        plt.plot(age, eva, label='eva')
+        plt.plot(age, hit_p, label='hit probability')
+        plt.plot(age, opp_cost, label='opportunity cost')
+
         plt.xlabel("age")
         plt.ylabel("eva")
         plt.ylim([-10, 5])
         plt.grid()
         # plt.ylim([2.5, 3])
-        # plt.legend(loc='lower right')
+        plt.legend(loc='lower right')
 
     plt.savefig(f'./img/img_{id}.png',
                 transparent = False,

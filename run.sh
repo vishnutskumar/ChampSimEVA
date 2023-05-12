b4da3182557973e05c691f09dd60631dafa0f3bd
CharlsBabu <charlespbabu@gmail.com>
2023-05-11 22:27:07 -0500

    final parameters for benchmark sweep

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-./config.sh  champsim_config.json
+./config.sh champsim_config.json
 make
-bin/champsim --warmup_instructions 60000 --simulation_instructions 400000 ../traces/ls_trace.champsim.gz
+
+bin/champsim --warmup_instructions 100000 --simulation_instructions 4000000 ../traces/CRC2_trace/astar_23B.trace.xz
 cd analysis
 python3 plot_AgevEVA.py
